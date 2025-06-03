2855d84ec3f5b693fff9cbce003a788f7caf2473
romain.bessede <romain.bessede@gmail.com>
2025-06-03 15:27:31 +0000

    Moitié dev about

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,43 +9,41 @@ const { t } = useI18n()
 <template>
     <section class="about" id="about">
       <div class="about-container">
-        <!-- Left Column (Text and Skills) -->
+        <div class="about-text">
+          <h2 v-html="t.aboutMe"></h2>
+          <p v-html="t.hello"></p>
+        </div>
+        <div class="lang-container-about">
+          <div class="lang-circle-about"></div>
+        </div>
         <div class="left-column">
-            <div class="about-text">
-                <h2 v-html="t.aboutMe"></h2>
-                <p v-html="t.hello"></p>
-            </div>
-
-            <!-- Skill Tags -->
             <div class="about-tags">
-                <div class="tag-block light">
+                <div class="tag-block front">
                     <h3 v-html="t.frontEnd"></h3>
                     <p>JavaScript / TypeScript / Vue / React / Angular</p>
                 </div>
 
-                <div class="tag-block">
+                <div class="tag-block styles">
                     <h3 v-html="t.styles"></h3>
                     <p>CSS / SCSS</p>
                 </div>
 
-                <div class="tag-block">
+                <div class="tag-block back">
                     <h3 v-html="t.backEnd"></h3>
                     <p>Node / Express / Symfony / Java / MySQL / PostgreSQL / MongoDB / Vertica</p>
                 </div>
 
-                <div class="tag-block">
+                <div class="tag-block ops">
                     <h3>DevOps</h3>
                     <p>Docker / Nginx / GitHub Actions / CI/CD / Bash / Vite</p>
                 </div>
             </div>
 
-            <div class="about-text">
+            <div class="about-text tech">
                 <p v-html="t.favoriteTech"></p>
             </div>
           </div>
 
-        <!-- Right Column (Photo) -->
-        <!-- Photo -->
         <div class="about-photo">
           <img src="@/assets/pp.jpg" alt="Photo de Romain" />
         </div>
